81986c69de8a9b494b40cbd111ffac6d478acd34
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-05 20:01:37 -0700

    create method screen

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ app = customtkinter.CTk()
 class Application:
     def __init__(self):
         self.app = app 
+        self.screen()
         app.mainloop()
+
+    def screen(self):
+        self.app.geometry("1920x1080")
+        self.app.resizable(True, True)
+        self.app.title("Manager Store")
 
 Application()
